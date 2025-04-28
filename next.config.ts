26a84aabafbc6a7fa7903496a5258bea355a8e7a
Mikel Ken <mayki6866@gmail.com>
2025-04-28 18:28:18 -0400

    feat(url):configuracion de url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Cualquier cosa que vaya a /api/...
+        destination: `${process.env.NEXT_PUBLIC_SOCKET_SERVER_URL}/api/:path*`, // Redirige al backend en HTTP
+      },
+    ];
+  },
 };
 
 export default nextConfig;
