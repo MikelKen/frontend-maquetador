7404728121d44773ba2970c2aca6b607721cdb77
Mikel Ken <mayki6866@gmail.com>
2025-04-28 20:07:18 -0400

    feat(ports):configuracion de puertos

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // Cualquier cosa que vaya a /api/...
-        destination: `${process.env.NEXT_PUBLIC_SOCKET_SERVER_URL}/api/:path*`, // Redirige al backend en HTTP
-      },
-    ];
-  },
+  // async rewrites() {
+  //   // return [
+  //   //   {
+  //   //     source: "/api/:path*", // Cualquier cosa que vaya a /api/...
+  //   //     destination: `${process.env.NEXT_PUBLIC_SOCKET_SERVER_URL}/api/:path*`, // Redirige al backend en HTTP
+  //   //   },
+  //   // ];
+  // },
 };
 
 export default nextConfig;
